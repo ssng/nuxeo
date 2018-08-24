b188b9f9b47ff5fda752bee601b07e60fd767351
Guillaume Renard <grenard@nuxeo.com>
2018-08-24 16:36:37 +0200

    NXP-25543: move collection to nuxeo-services

--- nuxeo-services/nuxeo-collections/src/test/java/org/nuxeo/ecm/collections/core/test/CollectionTestCase.java
+++ nuxeo-services/nuxeo-collections/src/test/java/org/nuxeo/ecm/collections/core/test/CollectionTestCase.java
@@ -34,7 +34,6 @@ import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.trash.TrashService;
 import org.nuxeo.ecm.core.work.api.WorkManager;
-import org.nuxeo.ecm.platform.test.PlatformFeature;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import org.nuxeo.runtime.transaction.TransactionHelper;
@@ -43,7 +42,7 @@ import org.nuxeo.runtime.transaction.TransactionHelper;
  * @since 5.9.3
  */
 @RunWith(FeaturesRunner.class)
-@Features({ PlatformFeature.class, CollectionFeature.class })
+@Features(CollectionFeature.class)
 public class CollectionTestCase {
 
     @Inject
